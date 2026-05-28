bb4a412e8697eb1f4d96584f78d95ee4f17102cf
Mikhail Zizganov <mzizganov@yandex.ru>
2026-05-28 21:28:48 +0300

    feat: добавил маршруты для клиентов и сообщений

---
+++ mailing/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'mailing'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('clients/', views.ClientListView.as_view(), name='client_list'),
+    path('clients/<int:pk>/', views.ClientDetailView.as_view(), name='client_detail'),
+    path('clients/create/', views.ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/update/', views.ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', views.ClientDeleteView.as_view(), name='client_delete'),
+]
