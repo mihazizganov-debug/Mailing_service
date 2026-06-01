f08ba2ac3cb3b01453709632697b7419845dd3ea
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-01 22:19:21 +0300

    fix: исправил ошибки PEP8 в admin.py

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,23 +1,28 @@
 from django.contrib import admin
-from .models import Client, Message, Mailing, Attempt
+
+from .models import Attempt, Client, Mailing, Message
+
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('email', 'full_name')
-    search_fields = ('email', 'full_name')
+    list_display = ("email", "full_name")
+    search_fields = ("email", "full_name")
+
 
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = ('subject',)
-    search_fields = ('subject',)
+    list_display = ("subject",)
+    search_fields = ("subject",)
+
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ('id', 'start_time', 'end_time', 'status', 'message')
-    list_filter = ('status',)
-    filter_horizontal = ('recipients',)
+    list_display = ("id", "start_time", "end_time", "status", "message")
+    list_filter = ("status",)
+    filter_horizontal = ("recipients",)
+
 
 @admin.register(Attempt)
 class AttemptAdmin(admin.ModelAdmin):
-    list_display = ('attempt_time', 'status', 'mailing')
-    list_filter = ('status',)
+    list_display = ("attempt_time", "status", "mailing")
+    list_filter = ("status",)
