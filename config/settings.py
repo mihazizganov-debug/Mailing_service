39622dc84dca56f0608b0177edad6680faed6d42
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-01 22:19:01 +0300

    feat: добавил настройки почты и форматы дат

--- config/settings.py
+++ config/settings.py
@@ -116,3 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'webmaster@localhost'
+
+USE_L10N = False
+DATE_INPUT_FORMATS = ['%d.%m.%Y %H:%M:%S', '%Y-%m-%d %H:%M:%S']
