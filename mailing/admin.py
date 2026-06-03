0f0616d30a73599f01180cd0587fac927ac429b2
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-03 22:31:28 +0300

    fix: исправил админку, убрал импорт User

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-
 from .models import Attempt, Client, Mailing, Message
 
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ("email", "full_name")
+    list_display = ("email", "full_name", "owner")
+    list_filter = ("owner",)
     search_fields = ("email", "full_name")
 
 
@@ -17,8 +17,8 @@ class MessageAdmin(admin.ModelAdmin):
 
 @admin.register(Mailing)
 class MailingAdmin(admin.ModelAdmin):
-    list_display = ("id", "start_time", "end_time", "status", "message")
-    list_filter = ("status",)
+    list_display = ("id", "start_time", "end_time", "status", "message", "owner")
+    list_filter = ("status", "owner")
     filter_horizontal = ("recipients",)
 
 
